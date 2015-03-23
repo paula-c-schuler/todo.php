5297c0b39b465d3a8ba13d11a1a75c93ecd26da4
Paula C Schuler <paulasatx@gmail.com>
2015-03-22 19:40:56 -0500

    Completed exercise, tidied file, functions nicely.

--- todo.php
+++ todo.php
@@ -1,40 +1,13 @@
 <?php
- //********* I WANT TO ADD AUTOMATIC CAPITALIZATION OF USER INPUT***
-
+ 
 // Create array to hold list of todo items
 $items = array();
 
-//add a function to follow input to sort
-function sort_menu($input, $items) {
-    echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: ";
-
-        //receive user input, trim and automatically capitalize
-    $inputS = strtoupper(trim(fgets(STDIN)));
-888888888888 STILL DOES NOT WORK 88888888888888
-        //perform what user requested
-    if ($inputS == 'A') {
-        asort($items);
-        return $items;
-        echo $items;
-
-    } elseif ($inputS == 'Z') {
-        arsort($items);
-        return $items;
-        echo $items;
-
-    } elseif ($inputS == 'O') {
-        ksort($items);
-        return $items;
-
-    } elseif ($inputS == 'R') {
-        krsort($items);
-        return $items;
-    } 
-}
-// The loop!
+// The loop starts here! 
 do {
     // Iterate through list items
     foreach ($items as $key => $item) {
+
         // Display each item and a newline
         echo "[{$key}] {$item}\n";
     }
@@ -47,37 +20,63 @@ do {
     $input = strtoupper(trim(fgets(STDIN)));
  
     // Check for actionable input
+    
     if ($input == 'N') {
-        // Ask for entry
-        echo 'Enter item: ';
-        // Add entry to list array
-        $items[] = trim(fgets(STDIN));
+        echo 'Add new item to (B)eginning of list OR (E)nd of list: ';
+        // Ask for choice of item placement on array of $items.
+        $inputBeginingOrEnd = strtoupper(trim(fgets(STDIN)));
+        
+        if ($inputBeginingOrEnd == 'B') {
+
+            // echo 'Enter item: ';
+            echo 'Enter item: ';
+            $inputNewItem = trim(fgets(STDIN));
+            
+            //Place trimmed item at first index of array;
+            array_unshift($items, $inputNewItem);
+
+        } else  {
+
+            // echo 'Enter item: ';
+            echo 'Enter item: ';
+            $inputNewItem = trim(fgets(STDIN));
+            
+            //Place trimmed item at the end of the array for choice 'E' and default no choice;
+            $items = $inputNewItem);
+        }
+
     } elseif ($input == 'R') {
+
         // Remove which item?
         echo 'Enter item number to remove: ';
+        
         // Get array key
         $key = trim(fgets(STDIN));
         // Remove from array
         unset($items[$key]);
+
     } elseif ($input == 'S') {
-
-        sort_menu($input, $items);
+        
         //add menu for sorting the array
- //    	echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered";
+    	echo "(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered";
 
- //        //receive user input, trim and automatically capitalize
- //    	$inputS = strtoupper(trim(fgets(STDIN)));
+        //receive user input, trim and automatically capitalize
+    	$inputS = strtoupper(trim(fgets(STDIN)));
 
- //        //perform what user requested
- //    	if ($inputS == 'A') {
-	// 	asort($items);
-	// } elseif ($inputS == 'Z') {
-	// 	arsort($items);
-	// } elseif ($inputS == 'O') {
-	// 	ksort($items);
-	// } elseif ($inputS == 'R') {
-	// 	krsort($items);
-	// } 
+        //perform what user requested
+    	if ($inputS == 'A') {
+		  asort($items);
+	   } elseif ($inputS == 'Z') {
+	   	   arsort($items);
+	   } elseif ($inputS == 'O') {
+	       ksort($items);
+	   } elseif ($inputS == 'R') {
+	       krsort($items);
+	   }       
+    } elseif ($input == 'F') {
+        array_shift($items);
+    } elseif ($input == 'L') {
+        array_pop($items);
     }
 // Exit when input is (Q)uit
 } while ($input != 'Q');
